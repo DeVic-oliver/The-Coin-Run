6be4923eee13eedfe7b289165c0c582e78855df6
Victor Matheus <victormatheus183@gmail.com>
2023-08-22 19:39:45 -0300

    Delegation for sceneloader and scene pauser

--- Assets/Scripts/Core/Management/SceneManagement/SceneManagerGateway.cs
+++ Assets/Scripts/Core/Management/SceneManagement/SceneManagerGateway.cs
@@ -1,5 +1,6 @@
 ﻿namespace Assets.Scripts.Core.Management.SceneManagement
 {
+    using System.Collections;
     using UnityEngine;
 
     public class SceneManagerGateway : MonoBehaviour
@@ -8,13 +9,35 @@
         private SceneLoader _sceneLoader;
 
         
+        public void PauseGame()
+        {
+            _scenePauser.PauseScene();
+        }
+
+        public void UnpauseGame()
+        {
+            _scenePauser.UnpauseScene();
+        }
+
         public void TogglePauseState()
         {
             _scenePauser.TogglePauseScene();
         }
 
-        public void RestartScene()
+        public void RestartSceneImmediately()
         {
+            _sceneLoader.RestartScene();
+        }
+
+        public void RestartSceneAfterThreeSeconds()
+        {
+            StartCoroutine("RestartSceneCoroutine");
+        }
+
+        private IEnumerator RestartSceneCoroutine()
+        {
+            float seconds = 3;
+            yield return new WaitForSecondsRealtime(seconds);
             _sceneLoader.RestartScene();
         }
 
@@ -23,6 +46,5 @@
             _scenePauser = new ScenePauser();
             _sceneLoader = new SceneLoader();
         }
-
     }
 }
