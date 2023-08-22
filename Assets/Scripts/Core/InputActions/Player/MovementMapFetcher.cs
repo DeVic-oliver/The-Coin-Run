9a109ffdffd22a67212d7cdcce4b890f9270256a
Victor Matheus <victormatheus183@gmail.com>
2023-08-21 23:11:37 -0300

    Fetches the movement actions map

---
+++ Assets/Scripts/Core/InputActions/Player/MovementMapFetcher.cs
@@ -0,0 +1,39 @@
+﻿namespace Assets.Scripts.Core.InputActions.Player
+{
+    using UnityEngine.InputSystem;
+
+    public class MovementMapFetcher
+    {
+        private InputActionAsset _actionAsset;
+
+
+        public MovementMapFetcher(InputActionAsset actionsAsset)
+        {
+            _actionAsset = actionsAsset;
+        }
+
+        public InputAction FetchHorizontalMoveInputAction()
+        {
+            InputActionMap map = FetchMovementActionMap();
+            return map.FindAction("Horizontal Movement");
+        }
+
+        public InputAction FetchVerticalMoveInputAction()
+        {
+            InputActionMap map = FetchMovementActionMap();
+            return map.FindAction("Vertical Movement");
+        }
+
+        public InputAction FetchJumpInputAction()
+        {
+            InputActionMap map = FetchMovementActionMap();
+            return map.FindAction("Jump");
+        }
+
+        private InputActionMap FetchMovementActionMap()
+        {
+            return _actionAsset.FindActionMap("Movement");
+        }
+
+    }
+}
