aeadea48ae90f0651b0c7f6d1368016154ce2fad
Victor Matheus <victormatheus183@gmail.com>
2023-08-22 19:08:06 -0300

    BasicScene management

---
+++ Assets/Scripts/Core/Management/SceneManagement/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿namespace Assets.Scripts.Core.Management.SceneManagement
+{
+    using UnityEngine.SceneManagement;
+    
+    public class SceneLoader
+    {
+        public int CurrentSceneBuildIndex { get; private set; }
+
+        private readonly int _fallbackSceneBuildIndex = 0;
+
+
+        public SceneLoader() 
+        {
+            SetCurrentSceneBuildIndex();
+        }
+
+        private void SetCurrentSceneBuildIndex()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            CurrentSceneBuildIndex = currentScene.buildIndex;
+        }
+
+        public void RestartScene()
+        {
+            SceneManager.LoadScene(CurrentSceneBuildIndex);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Core/Management/SceneManagement/SceneManagerGateway.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Scripts.Core.Management.SceneManagement
+{
+    using UnityEngine;
+
+    public class SceneManagerGateway : MonoBehaviour
+    {
+        private ScenePauser _scenePauser;
+        private SceneLoader _sceneLoader;
+
+        
+        public void TogglePauseState()
+        {
+            _scenePauser.TogglePauseScene();
+        }
+
+        public void RestartScene()
+        {
+            _sceneLoader.RestartScene();
+        }
+
+        private void Awake()
+        {
+            _scenePauser = new ScenePauser();
+            _sceneLoader = new SceneLoader();
+        }
+
+    }
+}
