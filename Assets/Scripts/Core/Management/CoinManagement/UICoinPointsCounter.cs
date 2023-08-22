1f65d116853130139c6d35c6d4138e3a00802a33
Victor Matheus <victormatheus183@gmail.com>
2023-08-22 16:46:26 -0300

    Updates the UI text for points

---
+++ Assets/Scripts/Core/Management/CoinManagement/UICoinPointsCounter.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts.Core.Management.CoinManagement
+{
+    using TMPro;
+    using UnityEngine;
+    
+    public class UICoinPointsCounter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _currentCoinsPointsTMP;
+
+
+        public void UpdateUICountPoints(int points)
+        {
+            _currentCoinsPointsTMP.text = points.ToString();
+        }
+
+    }
+}
