937450443eecf2974462ea4e6bf6a4873be774f6
Victor Matheus <victormatheus183@gmail.com>
2023-08-22 16:47:11 -0300

    Handles with data persistence of player best score

---
+++ Assets/Scripts/Core/Management/DataManagement/PlayerDataJSONParser.cs
@@ -0,0 +1,53 @@
+﻿namespace Assets.Scripts.Core.Management.DataManagement
+{
+    using System.IO;
+    using UnityEngine;
+    
+    public class PlayerDataJSONParser
+    {
+        public static void SaveBestscore(int totalScore) 
+        {
+            BestScoreData bestScore = GetBestScoreFromJSON();
+            if(bestScore.Points < totalScore)
+                OveverwriteBestScorePoints(bestScore, totalScore);
+        }
+
+        public static BestScoreData GetBestScoreFromJSON()
+        {
+            string path = GetJSONFilePath();
+            string json = File.ReadAllText(path);
+            return JsonUtility.FromJson<BestScoreData>(json);
+        }
+
+        private static void OveverwriteBestScorePoints(BestScoreData bestScoreData, int totalPoints)
+        {
+            bestScoreData.Points = totalPoints;
+            string path = GetJSONFilePath();
+            string updatedJson = JsonUtility.ToJson(bestScoreData, true);
+            File.WriteAllText(path, updatedJson);
+        }
+
+        private static string GetJSONFilePath()
+        {
+            string fileName = "bestscore.json";
+            string path = Path.Combine(Application.persistentDataPath, fileName);
+            
+            if (!File.Exists(path))
+                CreateJSONFile(path);
+            
+            return path;
+        }
+
+        private static void CreateJSONFile(string path)
+        {
+            string json = JsonUtility.ToJson(CreateNewData(), true);
+            File.WriteAllText(path, json);
+        }
+
+        private static BestScoreData CreateNewData(int bestScore = 0)
+        {
+            return new BestScoreData() { Points = bestScore };
+        }
+
+    }
+}
