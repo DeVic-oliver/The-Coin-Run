db1d244c578f2d5a156574476aa6a25289ef5f21
Victor Matheus <victormatheus183@gmail.com>
2023-08-22 22:09:25 -0300

    Basic pool for coins
    Prevents spawning unnecessary gameobjects

--- Assets/Scripts/Items/Coin/Coin.cs
+++ Assets/Scripts/Items/Coin/Coin.cs
@@ -6,8 +6,17 @@ namespace Assets.Scripts.Items
     [RequireComponent(typeof(Collider))]
     public class Coin : MonoBehaviour
     {
+        public CoinSpawner Spawner { get; set; }
+
         [SerializeField] private int _coinPoints;
 
+        [SerializeField] private float _rotationSpeed = 35f;
+
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up * _rotationSpeed * Time.deltaTime); 
+        }
 
         private void OnTriggerEnter(Collider other)
         {
@@ -20,7 +29,8 @@ namespace Assets.Scripts.Items
 
         private void OnDisable()
         {
-            //return to pool
+            if (Spawner != null)
+                Spawner.BackToPool(this);
         }
 
     }
---
+++ Assets/Scripts/Items/Coin/CoinSpawner.cs
@@ -0,0 +1,45 @@
+﻿namespace Assets.Scripts.Items
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [RequireComponent(typeof(CoinPool))]
+    public class CoinSpawner : MonoBehaviour
+    {
+        [SerializeField] private Coin _coin;
+        
+        private CoinPool _coinPool;
+
+
+        private void Awake()
+        {
+            _coinPool = GetComponent<CoinPool>();
+            _coinPool.InitPool(_coin);
+        }
+
+        public void BackToPool(Coin coin)
+        {
+            _coinPool.Pool.Release(coin);
+        }
+
+        private void Start()
+        {
+            StartCoroutine("SpawnCoins");
+        }
+        
+        private IEnumerator SpawnCoins()
+        {
+            float secondsToSpawn = 1.5f;
+            while (true)
+            {
+                yield return new WaitForSeconds(secondsToSpawn);
+                if(_coinPool.CurrentActiveAtMoment < _coinPool.MaxSize) 
+                { 
+                    Coin coin = _coinPool.Pool.Get();
+                    coin.Spawner = this;
+                    coin.gameObject.SetActive(true);
+                }
+            }
+        }
+
