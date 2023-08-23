ff2e612b64f2d889e26539af470a165c33cce967
Victor Matheus <victormatheus183@gmail.com>
2023-08-22 22:10:00 -0300

    Random coordinates to spawn coins based on Mn and Max coord

--- Assets/Scripts/Items/Coin/CoinSpawner.cs
+++ Assets/Scripts/Items/Coin/CoinSpawner.cs
@@ -8,6 +8,7 @@
     public class CoinSpawner : MonoBehaviour
     {
         [SerializeField] private Coin _coin;
+        [SerializeField] private List<MinMaxCoordinates> _coordinatesToSpawn;
         
         private CoinPool _coinPool;
 
@@ -38,8 +39,48 @@
                 { 
                     Coin coin = _coinPool.Pool.Get();
                     coin.Spawner = this;
+                    coin.gameObject.transform.position = GetRandomCoordinateYoSpawn();
                     coin.gameObject.SetActive(true);
                 }
             }
         }
 
+        private Vector3 GetRandomCoordinateYoSpawn()
+        {
+            int index = Random.Range(0, _coordinatesToSpawn.Count - 1);
+
+            float minX = GetMinCoordinatesFrom(index).x;
+            float maxX = GetMaxCoordinatesFrom(index).x;
+
+            float minY = GetMinCoordinatesFrom(index).y;
+            float maxY = GetMaxCoordinatesFrom(index).y;
+
+            float minZ = GetMinCoordinatesFrom(index).z;
+            float maxZ = GetMaxCoordinatesFrom(index).z;
+
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+            float randomZ = Random.Range(minZ, maxZ);
+
+            return new Vector3(randomX, randomY, randomZ);
+        }
+
+        private Vector3 GetMinCoordinatesFrom(int index)
+        {
+            return _coordinatesToSpawn[index].MinCoord;
+        }
+
+        private Vector3 GetMaxCoordinatesFrom(int index)
+        {
+            return _coordinatesToSpawn[index].MaxCoord;
+        }
+
+    }
+
+    [System.Serializable]
+    public class MinMaxCoordinates
+    {
+        public Vector3 MinCoord;
+        public Vector3 MaxCoord;
+    }
+}
