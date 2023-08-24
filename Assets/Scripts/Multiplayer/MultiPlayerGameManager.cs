4f786a7d59182b8b7f4704c8fd294fef399b5276
Victor Matheus <victormatheus183@gmail.com>
2023-08-24 00:25:45 -0300

    Host and Guest setting up on Multiplayer Game Manager

--- Assets/Scripts/Multiplayer/MultiPlayerGameManager.cs
+++ Assets/Scripts/Multiplayer/MultiPlayerGameManager.cs
@@ -5,17 +5,30 @@
     using Assets.Scripts.Core.Management.SceneManagement;
     using Photon.Realtime;
     using TMPro;
+    using Assets.Scripts.Core;
+
 
     public class MultiPlayerGameManager : MonoBehaviourPunCallbacks
     {
         [SerializeField] private SceneManagerGateway _sceneGateway;
 
-        [SerializeField] private Transform _spawn1;
-        [SerializeField] private Transform _spawn2;
-        [SerializeField] private GameObject _playerPrefabResource;
 
-        [SerializeField] private TextMeshProUGUI _masterNickName;
-        [SerializeField] private TextMeshProUGUI _guestNickName;
+        [Header("Master Setup")]
+        [SerializeField] private Transform _spawn1;
+        [SerializeField] private TextMeshProUGUI _masterNickTMP;
+        [SerializeField] private GameObject _masterPrefabResource;
+        [Header("Master Dependencies")]
+        [SerializeField] private TextMeshProUGUI _masterScoreTMP;
+        private CoinDetector _masterCoinDetector;
+
+        [Header("Guest Setup")]
+        [SerializeField] private Transform _spawn2;
+        [SerializeField] private TextMeshProUGUI _guestNickTMP;
+        [SerializeField] private GameObject _guestPrefabResource;
+        [Header("Guest Dependencies")]
+        [SerializeField] private TextMeshProUGUI _guestScoreTMP;
+        private CoinDetector _guestCoinDetector;
+
 
 
         public override void OnLeftRoom()
@@ -47,23 +60,47 @@
             PhotonNetwork.LoadLevel("Arena Multiplayer");
         }
 
-        private void Start()
+        private void Awake()
         {
+            InjectPlayersDependencies();
             SetPlayerInTheirSpawnPositions();
-            _masterNickName.text = GetMasterPlayer().NickName;
-            _guestNickName.text = GetGuestPlayer().NickName;
+        }
+
+        private void InjectPlayersDependencies()
+        {
+            InjectMasterDependencies();
+            InjectGuestDependencies();
+        }
+
+        private void InjectMasterDependencies()
+        {
+            _masterCoinDetector = _masterPrefabResource.GetComponent<CoinDetector>();
+            _masterCoinDetector.CoinsCollectedTMP = _masterScoreTMP;
+        }
+
+        private void InjectGuestDependencies()
+        {
+            _guestCoinDetector = _guestPrefabResource.GetComponent<CoinDetector>();
+            _guestCoinDetector.CoinsCollectedTMP = _guestScoreTMP;
         }
 
         private void SetPlayerInTheirSpawnPositions()
         {
             if (PhotonNetwork.IsMasterClient)
             {
-                PhotonNetwork.Instantiate(_playerPrefabResource.name, _spawn1.position, _spawn1.rotation);
+                PhotonNetwork.Instantiate(_masterPrefabResource.name, _spawn1.position, _spawn1.rotation);
             }
             else
             {
-                PhotonNetwork.Instantiate(_playerPrefabResource.name, _spawn2.position, _spawn2.rotation);
+                PhotonNetwork.Instantiate(_guestPrefabResource.name, _spawn2.position, _spawn2.rotation);
             }
+        }
+
+        private void Start()
+        {
+
+            _masterNickTMP.text = GetMasterPlayer().NickName;
+            _guestNickTMP.text = GetGuestPlayer().NickName;
         }
 
         private Player GetMasterPlayer()
